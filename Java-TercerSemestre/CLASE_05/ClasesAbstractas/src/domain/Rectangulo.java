c2f989761b568eddf19324ed70bd9bb5f7562dba
VALENTIN20241 <valentinherrera589@gmail.com>
2025-05-15 20:40:00 -0300

    Agrego clases 02 a 05 de Java - Tercer Semestre

---
+++ Java-TercerSemestre/CLASE_05/ClasesAbstractas/src/domain/Rectangulo.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class Rectangulo extends FiguraGeometrica {
+    // Constructor
+    public Rectangulo(String tipoFigura) {
+        super(tipoFigura);
+    }
+
+    @Override
+    public void dibujar() {
+        // Implementando el método abstracto
+        System.out.println("Se imprime un: " + this.getClass().getSimpleName());
+    }
+    
+}
