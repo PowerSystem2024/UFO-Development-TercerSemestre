70ded8afa3369d240ee8168d938f2427bc4c4bcc
Dario Benitez <benitezdario007@gmail.com>
2025-05-27 15:13:01 -0300

    Agrego CLASE_07 de Java

---
+++ Java-TercerSemestre/CLASE_07/Interfaces/src/accesodatos/ImplementacionMySql.java
@@ -0,0 +1,26 @@
+
+package accesodatos;
+
+public class ImplementacionMySql implements IAccesoDatos{
+
+    @Override
+    public void insertar() {
+        System.out.println("Insertar desde MySql");
+    }
+
+    @Override
+    public void listar() {
+        System.out.println("Insertar desde MySql");
+    }
+
+    @Override
+    public void actualizar() {
+        System.out.println("Insertar desde MySql");
+    }
+
+    @Override
+    public void eliminar() {
+        System.out.println("Insertar desde MySql");
+    }
+    
+}
